32d5f122908ecc6eeb1027177bd5573a971409f7
Dhruvil-cyber <63727361+Dhruvil-cyber@users.noreply.github.com>
2025-09-24 00:28:23 +1000

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:5000", // for local dev only
+        target: "http://127.0.0.1:5000", // dev only
         changeOrigin: true,
-      }
-    }
-  }
+      },
+    },
+  },
 });
